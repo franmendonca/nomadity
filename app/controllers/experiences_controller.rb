bd99a8f90e351461aaac50323271719d3fbdb637
Francisco Mendonca <franciscomendonca8@gmail.com>
2022-03-01 15:27:14 +0000

    added experiences routes for show and index

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,14 @@
 class ExperiencesController < ApplicationController
+  before_action :set_experience, only: [:show]
+  def index
+    @experiences = Experience.all
+  end
+
+  def show; end
+
+  private
+
+  def set_experience
+    @experience = Experience.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :experiences, only: [:index, :show]
   resources :categories, only: [:index]
   resources :profile, only: [:show]
-  
+
 
 end
