3c60fbcd10a55a006e058e338adeec4109d685b6
Riley Chapman <chapman.riley@gmail.com>
2022-03-03 15:50:53 +0000

    Added mapbox to results page

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,5 +2,8 @@ class Experience < ApplicationRecord
   belongs_to :city
   belongs_to :category
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   acts_as_favoritable
 end
