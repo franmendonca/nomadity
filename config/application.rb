8614bdbef649739c4714f7ba6f0199a62a25cae8
Revdep <sbodezzi@gmail.com>
2022-03-08 14:11:38 +0100

    created error pages

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Nomadity
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.exceptions_app = self.routes
   end
 end
