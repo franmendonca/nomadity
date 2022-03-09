f8fe3a9e5806a819b912f4ba34f3b8781864e82a
Riley Chapman <chapman.riley@gmail.com>
2022-03-09 10:36:41 +0000

    removed search bar on mapbox

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -18,10 +18,6 @@ export default class extends Controller {
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
 
-    this.map.addControl(new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl
-    }))
   }
 
   #addMarkersToMap() {
